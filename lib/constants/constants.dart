4d6d10d5e823e26937d9ce5158cab23449f65aa4
Muhammad Wasif Sheikh <sheikhmohammadrauf@gmail.com>
2022-09-11 13:02:04 +0500

    Added Login Screen

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class Constants {
@@ -22,3 +24,35 @@ class Constants {
 //     MaterialPageRoute(builder: (context) => page()),
 //   );
 // }
+Widget textfeild(String text1, String text2, String text3, String image) {
+  return Column(
+    children: [
+      SizedBox(
+        height: 30,
+      ),
+      Padding(
+        padding: const EdgeInsets.only(right: 325),
+        child: Text(text1),
+      ),
+      Padding(
+        padding: const EdgeInsets.only(right: 340),
+        child: Text(
+          text2,
+          style: TextStyle(color: Color(0xff004A61)),
+        ),
+      ),
+      SizedBox(
+        height: 10,
+      ),
+      Container(
+        width: 280,
+        decoration: BoxDecoration(color: Colors.white),
+        child: TextFormField(
+          decoration: InputDecoration(
+              prefixIcon: Image(height: 20, image: AssetImage(image)),
+              hintText: text3),
+        ),
+      )
+    ],
+  );
+}
